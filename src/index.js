63acbd5d297a7d56969dbad5278aa67eca365a84
NewAnton <1anton1korobov@gmail.com>
2021-08-18 00:55:26 +0300

    feat: easy peasy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined || array.length === 0)
+    return 0
+  let minValue = 0
+  for (let i = 0; i < (array.length); i++) {
+    if (array[i] < minValue) {
+      minValue = array[i]
+    }
+  }
+  return minValue;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0)
+    return 0
+  let maxValue = 0
+  for (let i = 0; i < (array.length); i++) {
+    if (array[i] > maxValue) {
+      maxValue = array[i]
+    }
+  }
+  return maxValue;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined || array.length === 0)
+    return 0
+  let avgValue = 0
+  const reducer = (accumulator, currentValue) => accumulator + currentValue
+  avgValue = array.reduce(reducer) / (array.length)
+  return avgValue;
 }
